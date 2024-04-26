ed523b83dbc995875ce605d1f7da1585abc88e26
Faria-Anjum <114348170+Faria-Anjum@users.noreply.github.com>
2024-04-26 23:18:39 +0600

    updated all files

--- mealdb.js
+++ mealdb.js
@@ -12,12 +12,58 @@ function display(items){
     var oldContent = document.getElementById("row");
     oldContent.textContent =  '';
 
-    for (var i=0; i<items.length; i++){
-        var newDiv = document.createElement("div");
-        console.log(items[i]);
-        newDiv.innerHTML = `ID: ${items[i].idMeal}`;
+    if (items.length<5){
+        for (var i=0; i<items.length; i++){
+            var newDiv = document.createElement("div");
+            console.log(items[i]);
+            newDiv.innerHTML = `<img src=${items[i].strMealThumb} alt="" class="card-img-top">
+                                <div class="card-body">
+                                    <h3 class="card-title">${items[i].strMeal}</h5>
+                                    <h5> ID: ${items[i].idMeal}</h4>
+                                    <p class="card-text">${items[i].strInstructions}</p>
+                                </div>
+                                `;
+            newDiv.classList.add("card", "col", "col-lg-4", "col-md-6", "col-12", "p-3", "mx-auto", "mb-3", "style");
+            oldContent.appendChild(newDiv);
+        }
     }
 
-    newDiv.classList.add("style");
-    oldContent.appendChild(newDiv);
+    else{
+        for (var i=0; i<5; i++){
+            var newDiv = document.createElement("div");
+            console.log(items[i]);
+            newDiv.innerHTML = `<img src=${items[i].strMealThumb} alt="" class="card-img-top">
+                                <div class="card-body">
+                                    <h3 class="card-title">${items[i].strMeal}</h5>
+                                    <h5> ID: ${items[i].idMeal}</h4>
+                                    <p class="card-text">${items[i].strInstructions}</p>
+                                </div>
+                                `;
+            newDiv.classList.add("card", "col", "col-lg-4", "col-md-6", "col-12", "p-3", "mx-auto", "mb-3", "style");
+            oldContent.appendChild(newDiv);
+        }
+
+        var button = document.createElement("div");
+        button.innerHTML = `<button onclick="showAll(5)" class="btn btn-default btn-lg my-2 my-sm-0 mx-2 showall">Show All</button>`;
+        button.classList.add("button");
+        oldContent.appendChild(button);
+    }
+}
+
+function showAll(i){
+    var oldContent = document.getElementById("row");
+    
+    for (var i=5; i<items.length; i++){
+        var newDiv = document.createElement("div");
+        console.log(items[i]);
+        newDiv.innerHTML = `<img src=${items[i].strMealThumb} alt="" class="card-img-top">
+                            <div class="card-body">
+                                <h3 class="card-title">${items[i].strMeal}</h5>
+                                <h5> ID: ${items[i].idMeal}</h4>
+                                <p class="card-text">${items[i].strInstructions}</p>
+                            </div>
+                            `;
+        newDiv.classList.add("card", "col", "col-lg-4", "col-md-6", "col-12", "p-3", "mx-auto", "mb-3", "style");
+        oldContent.appendChild(newDiv);
+    }
 }
